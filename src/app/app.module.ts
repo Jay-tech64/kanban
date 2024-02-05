dc75ffb1dbc2b2b2950ce0196772a38e932a46e9
Jérémy Mailhot <jaytek64@gmail.com>
2024-02-05 07:31:15 -0500

    Added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
