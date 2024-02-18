4c1a773d6e5d2afb10995767dea1bd427fe20b3b
Jérémy Mailhot <jaytek64@gmail.com>
2024-02-18 14:21:53 -0500

    Completed "Firebase Setup"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,36 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+
+import { environment } from '../environments/environment.development';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [provideAnimationsAsync()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+  ],
+  providers: [
+    provideAnimationsAsync(),
+    ScreenTrackingService,
+    UserTrackingService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDdB3qW2nJIKbdp4iLv1O2WffP0f0AH0Cc',
+    authDomain: 'kanban-b74eb.firebaseapp.com',
+    projectId: 'kanban-b74eb',
+    storageBucket: 'kanban-b74eb.appspot.com',
+    messagingSenderId: '350910686596',
+    appId: '1:350910686596:web:bda5b8b9f3107f288869d1',
+    measurementId: 'G-J6RHC26KR8',
+  },
+};
