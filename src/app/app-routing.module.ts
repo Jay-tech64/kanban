84eddae341097c60ba5850ebfbf47775d94a7a79
Jérémy Mailhot <jaytek64@gmail.com>
2024-02-21 21:24:46 -0500

    Completed Lazy Loaded Login Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomePageComponent,
+  },
+  {
+    path: "login",
+    loadChildren: () => import("./user/user.module").then((m) => m.UserModule),
   },
 ];
 
