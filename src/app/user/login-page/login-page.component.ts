f28c446f838eef60a0a7a94c1b8b84002e2382f4
Jérémy Mailhot <jaytek64@gmail.com>
2024-03-03 22:50:30 -0500

    Completed Google SignIn

--- src/app/user/login-page/login-page.component.ts
+++ src/app/user/login-page/login-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrl: "./login-page.component.scss",
 })
 export class LoginPageComponent {
-
+  constructor(public afAuth: AngularFireAuth) {}
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from "@angular/common";
 
 import { UserRoutingModule } from "./user-routing.module";
 import { SharedModule } from "../shared/shared.module";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { GoogleSigninDirective } from "./google-signin.directive";
+import { FIREBASE_OPTIONS } from "@angular/fire/compat";
+import { environment } from "../../environments/environment.development";
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginPageComponent, GoogleSigninDirective],
   imports: [CommonModule, UserRoutingModule, SharedModule],
+  providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
 })
 export class UserModule {}
